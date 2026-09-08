2573ed089bba69ca42f7ec24a2c4fac7f2026bb6
Gameknight963 <88798282+Gameknight963@users.noreply.github.com>
2026-09-08 18:56:54 -0500

    switch it to fastcall because it makes more sense to me

--- seashell/module.c
+++ seashell/module.c
@@ -1,18 +1,18 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
-static PyObject* hello(PyObject* self, PyObject* args) {
+static PyObject* hello(PyObject* self, PyObject* const* args, Py_ssize_t nargs) {
     return PyUnicode_FromString("hello from C!");
 }
 
 static PyMethodDef methods[] = {
-    {"hello", hello, METH_VARARGS, "Returns a greeting"},
+    {"hello", hello, METH_FASTCALL, "Returns a greeting"},
     {NULL, NULL, 0, NULL}
 };
 
 static struct PyModuleDef module = {
     PyModuleDef_HEAD_INIT,
-    "mymodule",
+    "seashell",
     NULL,
     -1,
     methods
