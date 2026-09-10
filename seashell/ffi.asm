c598db653da7f9d015e2083d5c6cbd5f5765674e
Gameknight963 <88798282+Gameknight963@users.noreply.github.com>
2026-09-09 17:23:55 -0700

    partial ffi implementation

--- seashell/ffi.asm
+++ seashell/ffi.asm
@@ -15,25 +15,47 @@ PUBLIC asm_call
 .code
 asm_call PROC
     ; we're pushing 4 values
-    ; 32 (shadow space) + 32 (our sub) + 8 (return address) = 72
+    ; 32 (shadow space) + 32 (our locals) + 8 (return address) = 72
     ; we need one more byte to get to a 16 byte aligned number, 80
     ; so we sub 40 from rsp
 
     sub rsp, 40
-    push rcx
-    push rdx
-    push r8
-    push r9
+    push rcx ; [rsp - 24]
+    push rdx ; [rsp - 16]
+    push r8 ; [rsp - 8]
+    push r9 ; [rsp]
 
     ; jump to done if arg count is less than 1
-    cmp rdx, 1
-    jl done
 
-asm_call ENDP
+    ; first item of argv
+    cmp [rsp + 16], 1 ; rsp + 16 is argc
+    jl done ; jump if argc is 0
+    mov r10, [rsp + 8] ; rsp + 8 is argv pointer
+    mov rcx, [r10]
+
+    ; second item of argv
+    cmp [rsp + 16], 2 ; rsp + 16 is argc
+    jl done ; jump if argc is 1
+    mov r10, [rsp + 8] ; rsp + 8 is argv pointer
+    mov rdx, [r10 + 8] ; argv item size is 64 bit
+    
+    ; third item of argv
+    cmp [rsp + 16], 3
+    jl done
+    mov r10, [rsp + 8]
+    mov r8, [r10 + 16]
+
+    ; fourth item of argv
+    cmp [rsp + 16], 4
+    jl done
+    mov r10, [rsp + 8]
+    mov r9, [r10 + 24]
 
     done:
-        call ; fill in later, cant use rcx
+        call [rsp + 24]
         add rsp, 40
         ret
+
+asm_call ENDP
 
 END
