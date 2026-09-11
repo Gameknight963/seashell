429c7682a39cb22d9820988b9f3e3237fb88f444
Gameknight963 <88798282+Gameknight963@users.noreply.github.com>
2026-09-11 08:53:42 -0700

    comment out MessageBeep call for now

--- DemoApplication/DemoApplication.py
+++ DemoApplication/DemoApplication.py
@@ -3,5 +3,5 @@ from seashell import *
 kernel32: Dll = Dll("kernel32.dll")
 kernel32.Beep(440, 500)
 
-user32: Dll = Dll("user32.dll")
-user32.MessageBeep(0)
+# user32: Dll = Dll("user32.dll")
+# user32.MessageBeep(0)
